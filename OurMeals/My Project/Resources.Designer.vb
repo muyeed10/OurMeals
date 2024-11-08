9412ef03bffb1c7c3efb166a542137d80ee1f517
Zakir Dawood <zakirhdawood@gmail.com>
2024-11-07 22:20:16 -0500

    changing pic resources

--- OurMeals/My Project/Resources.Designer.vb
+++ OurMeals/My Project/Resources.Designer.vb
@@ -73,26 +73,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property notification() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("notification", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property notification1() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("notification1", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property notificationbell() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("notificationbell", resourceCulture)
